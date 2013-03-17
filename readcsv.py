14f4e1eedad738e6070715958c544c212af7a009
Dario Bertini <berdario@gmail.com>
2013-03-17 12:41:35 +0100

    Added afterquery and set up a minimal http server to supply it the json data extracted from the csv

--- readcsv.py
+++ readcsv.py
@@ -1,13 +1,35 @@
+#! /usr/bin/env python3
+
 from csv import reader
-from collections import Counter
-from decimal import Decimal
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from json import dumps
+from sys import argv
+from os import path
+import webbrowser
 
-spese = Counter()
+PORT = 9000
 
-with open("spese.csv", encoding="utf-8") as f:
-    for line in reader(f):
-        spese.update({d:Decimal(c) for d,c,_ in zip(filter(None, line[2::3]), line[3::3], line[4::3])})
+def csv_to_json():
+    spese = []
+    with open(argv[1], encoding="utf-8") as f:
+        for line in reader(f):
+            spese.extend((line[0], desc, cost, name or "all") for desc,cost,name in zip(filter(None, line[2::3]), line[3::3], line[4::3]))
 
-for key in sorted(spese, key=spese.get):
-    print(key, spese[key])
-    
+    return dumps([("date", "desc", "cost", "name")] + spese).encode()
+
+class JsonHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(b'jsonp(' + csv_to_json() + b')')
+        return
+
+
+url = "file://" + path.join(path.dirname(path.abspath(__file__)), "afterquery", "render.html") + "#url=http://localhost:{0}{{}}&title={1}".format(PORT, path.basename(argv[1]))
+
+webbrowser.open(url.format("&treegroup=name,desc;cost&chart=tree"))
+webbrowser.open(url.format("&pivot=date;name;cost&chart=stacked"))
+
+httpd = HTTPServer(('', PORT), JsonHandler)
+httpd.serve_forever()
